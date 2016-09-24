5d61bcd2197f94559e70302b60f544a0cb3245c1
Edgar Cherkasov <edgar.cherkasov@yandex.ru>
2016-09-24 20:03:43 +0300

    Add code for radio frames support (not tested yet).

---
+++ MStation/Frame.c
@@ -0,0 +1,55 @@
+#include "Frame.h"
+
+uint8_t frame_generate(void *data, struct frame *fr, uint8_t data_len,
+	uint8_t type)
+{
+	uint8_t crc = 0xff, i;
+
+	if (data_len > FRAME_PAYLOAD_LEN)
+		return FRAME_ERR_LEN;
+
+	if ((type < FRAME_START) || (type > FRAME_SIMPLE))
+		return FRAME_ERR_TYPE;
+
+	fr->header = type << FRAME_TYPE_OFFS;
+	fr->header |= data_len;
+
+	memcpy(fr->payload, data, data_len);
+
+	crc = frame_crc_calc(crc, fr->header);
+	for (i = 0; i < data_len; i++)
+	{
+		crc = frame_crc_calc(crc, data[i]);
+	}
+
+	fr->checksum = crc;
+
+	return FRAME_OK;
+}
+
+uint8_t frame_decode(struct frame *fr, void *data, uint8_t *data_len,
+	uint8_t *type)
+{
+	uint8_t data_crc = 0xff, i;
+
+	*data_len = FRAME_LEN(fr);
+	*type = FRAME_TYPE(fr);
+
+	data_crc = frame_crc_calc(data_crc, fr->header);
+	for (i = 0; i < *data_len; i++) {
+		data_crc = frame_crc_calc(data_crc, fr->data[i]);
+	}
+
+	if (data_crc == fr->checksum) {
+		memcpy(data, fr->payload, data_len);
+	} else {
+		return FRAME_ERR_CRC;
+	}
+
+	return FRAME_OK;
+}
+
+static frame_crc_calc(uint8_t crc, uint8_t data)
+{
+	return _crc8_ccitt_update(crc, data);
+}
---
+++ MStation/Frame.h
@@ -0,0 +1,35 @@
+#include <util/crc16.h>
+
+#define FRAME_PAYLOAD_LEN 30
+#define FRAME_TYPE_OFFS 5
+#define FRAME_TYPE_MASK 0xE0
+#define FRAME_LEN_MASK 0x1F
+
+enum FRAME_TYPE
+{
+	FRAME_START = 0x4,
+	FRAME_REGULAR = 0x5,
+	FRAME_END = 0x6,
+	FRAME_SIMPLE = 0x7
+};
+
+enum FRAME_ERR
+{
+	FRAME_OK = 0,
+	FRAME_ERR_LEN = 1, // data length too long
+	FRAME_ERR_TYPE, // wrong frame type
+	FRAME_ERR_CRC
+};
+
+struct frame {
+	uint8_t header;
+	uint8_t payload[FRAME_PAYLOAD_LEN];
+	uint8_t checksum;
+};
+
+#define FRAME_TYPE(A) (((A)->header & FRAME_TYPE_MASK) >> FRAME_TYPE_OFFS)
+#define FRAME_LEN(A) ((A)->header & FRAME_LEN_MASK)
+
+uint8_t frame_generate(void *, struct frame *, uint8_t, uint8_t, uint8_t);
+uint8_t frame_decode(struct frame *, void *, uint8_t *, uint8_t *);
+static frame_crc_calc(uint8_t, uint8_t);
