29113ea01a50f6ec62987b51da82c3addc365ad2
Edgar Cherkasov <edgar.cherkasov@yandex.ru>
2017-04-09 22:39:08 +0300

    tests: various improvments in func test.
     - sync with Frame changes;
     - add MData packet raw print;
     - fix packet restoring issue.

--- unit_tests/unit_test.ino
+++ unit_tests/unit_test.ino
@@ -13,9 +13,10 @@ uint8_t out_data[FRAME_PAYLOAD_LEN];
 struct frame t_frame;
 struct datetime meas_time;
 struct mdata_packet test_packet;
-uint8_t test_frames_buf[BUF_SIZE];
 struct frames_buffer t_fbuf;
+struct frames_buffer meas_t_fbuf;
 uint8_t t_packet_buf[sizeof(struct mdata_packet)];
+uint8_t *t_packet_buf_ptr;
 uint8_t t_packet_data_len = 0;
 uint8_t t_packet_data_free = sizeof(struct mdata_packet);
 
@@ -29,6 +30,7 @@ uint8_t test_humidity = 78;
 int tests_run = 0;
 int tests_failed = 0;
 
+void print_raw_data(void *, uint16_t);
 void check_test(int8_t, const char *, const char*);
 void print_time(unsigned long, unsigned long, const char *);
 struct datetime conv_time(time_t);
@@ -59,10 +61,10 @@ void loop()
 	check_test(res, "MDATA_ADD_TEMP", "");
 	res = mdata_add_temp(&test_temp2, MDATA_TEMP2);
 	check_test(res, "MDATA_ADD_TEMP2", "");
-	//res = mdata_add_temp(&test_temp3, MDATA_TEMP3);
-	//check_test(res, "MDATA_ADD_TEMP3", "");
-	//res = mdata_add_temp(&test_temp4, MDATA_TEMP4);
-	//check_test(res, "MDATA_ADD_TEMP4", "");
+	res = mdata_add_temp(&test_temp3, MDATA_TEMP3);
+	check_test(res, "MDATA_ADD_TEMP3", "");
+	res = mdata_add_temp(&test_temp4, MDATA_TEMP4);
+	check_test(res, "MDATA_ADD_TEMP4", "");
 	res = mdata_add_humidity(&test_humidity, MDATA_HUMIDITY);
 	check_test(res, "MDATA_ADD_HUMID", "");
 	res = mdata_add_pressure(&test_pressure, MDATA_PRESSURE);
@@ -70,28 +72,34 @@ void loop()
 	mdata_fin_packet(&test_packet);
 	check_test(test_packet.header.length, "PACKET_DATA_LENGHT", "");
 	check_test(mdata_packet_len(&test_packet), "PACKET_LENGHT", "");
-	//check_test(test_packet.header.checksum, "PACKET_CRC", "");
+	check_test(test_packet.header.checksum, "PACKET_CRC", "");
 	/* Check packet */
 	EXEC_TIME(res = mdata_check_packet(&test_packet), "CHECK_PACKET");
 	check_test(res, "CHECK_PACKET", "");
+	/* Print packet data in raw format */
+	Serial.print("PACKET_RAW:\n");
+	print_raw_data(&test_packet, mdata_packet_len(&test_packet));
+	Serial.println();
 	
 	/* Test frame generaton from packet */
 	frame_cnt = frame_data_to_frames(&test_packet, mdata_packet_len(&test_packet),
-		test_frames_buf, BUF_SIZE);
+		&meas_t_fbuf, FRAMES_BUF_LEN);
 	check_test(frame_cnt, "PACKET_TO_FRAMES", "");
 
 	/* Test checking of frame buffer on base */
 	res = frame_get_stream(&t_fbuf, t_packet_buf, t_packet_data_free, &data_len);
 	check_test(res, "CHECK_FBUF", "");
 	EXEC_TIME(frame_get_stream(&t_fbuf, t_packet_buf, t_packet_data_free, &data_len), "CHECK_FBUF");
+	Serial.println();
 
+	/* Test frame buffer on base */
 	t_fbuf.tail = 0;
 	t_fbuf.head = 0;
-	/* Test frame buffer on base */
+	// Copy measure module data to base station buffer and measure copy time
 	for (i = 0; i < frame_cnt; i++) {
 		st_time = micros();
 		memcpy(&t_fbuf.frame_record[t_fbuf.tail],
-			test_frames_buf + (i * sizeof(struct frame)), sizeof(struct frame));
+			&meas_t_fbuf.frame_record[i], sizeof(struct frame));
 		FRAME_BUF_PTR_INC(t_fbuf.tail);
 		end_time = micros();
 		print_time(st_time, end_time, "FRAME_COPY");
@@ -99,17 +107,31 @@ void loop()
 	}
 
 	/* Check frame buffer for incoming packets */
+	t_packet_buf_ptr = t_packet_buf;
 	do {
 		check_test(t_fbuf.head, "FBUF_HEAD", "");
-		EXEC_TIME((res = frame_get_stream(&t_fbuf, t_packet_buf, t_packet_data_free, &data_len)), "GET_FBUF");
+		EXEC_TIME(
+			(res = frame_get_stream(
+				&t_fbuf, t_packet_buf_ptr,
+				t_packet_data_free, &data_len)
+			),
+			"GET_FBUF");
 		check_test(res, "GET_FBUF", "");
 		Serial.print("Take DATA size = ");
 		Serial.println(data_len);
+		t_packet_buf_ptr += data_len;
+		t_packet_data_free -= data_len;
 	} while (res != FRAME_BUF_EMPTY);
+
+	Serial.println();
 
 	/* Check packet */
 	EXEC_TIME(res = mdata_check_packet((struct mdata_packet *) t_packet_buf), "CHECK_PACKET");
 	check_test(res, "CHECK_PACKET", "");
+	/* Print packet data in raw format */
+	Serial.print("PACKET_RAW:\n");
+	print_raw_data(&test_packet, mdata_packet_len(&test_packet));
+	Serial.println();
 
 	/* Testing frame generation */
 	res = frame_generate(test_data, &t_frame, sizeof(test_data), FRAME_START);
@@ -130,6 +152,17 @@ void loop()
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	delay(10);
 	sleep_mode();
+}
+
+void print_raw_data(void *data, uint16_t len)
+{
+	uint16_t i;
+
+	for (i = 0; i < len; i++) {
+		Serial.print(*((uint8_t *) data + i), HEX);
+		Serial.print(' ');
+	}
+	Serial.print('\n');
 }
 
 void check_test(int8_t res, const char *test_name, const char *err_msg)
